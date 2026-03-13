af235c4e62ee4d259235fc7fb0b02e14b70efa6f
Luiz Eduardo <luizeduardo1953@gmail.com>
2026-03-13 11:43:40 -0300

    Changes discipline.controller

--- backend/src/discipline/controllers/discipline.controller.ts
+++ backend/src/discipline/controllers/discipline.controller.ts
@@ -1,36 +1,44 @@
-import { Controller } from "@nestjs/common";
-import { DisciplineRepository } from "../domain/repositories/DisciplineRepository";
+import { Body, Controller, HttpCode, HttpStatus } from "@nestjs/common";
 import { Discipline } from "../domain/entities/Discipline";
 import { DisciplinePrismaRepository } from "../infra/DisciplinePrismaRepository";
+import { CreateDisciplineDto } from "../dto/create-discipline.dto";
+import { UpdateDisciplineDto } from "../dto/update-discipline.dto";
+import { Roles } from "src/auth/decorators/roles.decorator";
 
-import { Post, Get, Put, Delete } from "@nestjs/common";
+import { Post, Get, Put, Delete, Param } from "@nestjs/common";
+import { Role } from "src/enums/role";
 
 @Controller('discipline')
 export class DisciplineController {
     constructor(private readonly disciplineRepository: DisciplinePrismaRepository) {}
 
+    @HttpCode(201)
     @Post()
-    async create(name: string): Promise<Discipline> {
-        return this.disciplineRepository.create(name);
+    async create(@Body() data: CreateDisciplineDto): Promise<Discipline> {
+        return this.disciplineRepository.create(data.name);
     }
 
+    @Roles(Role.Admin)
     @Get()
     async findAll(): Promise<Discipline[]> {
         return this.disciplineRepository.findAll();
     }
 
+    @Roles(Role.Admin)
     @Put()
-    async update(name: string, newName: string): Promise<Discipline> {
-        return this.disciplineRepository.update(name, newName);
+    async update(@Body() data: UpdateDisciplineDto, newName: string): Promise<Discipline> {
+        return this.disciplineRepository.update(data.name, newName);
     }
 
-    @Delete()
-    async delete(name: string): Promise<void> {
+    @Roles(Role.Admin)
+    @Delete(':name')
+    async delete(@Param('name') name: string): Promise<void> {
         await this.disciplineRepository.delete(name);
     }
 
-    @Get()
-    async findByName(name: string): Promise<Discipline | null> {
+    @Roles(Role.Student, Role.Teacher)
+    @Get(':name')
+    async findByName(@Param('name') name: string): Promise<Discipline | null> {
         return this.disciplineRepository.findByName(name);
     }
 }
